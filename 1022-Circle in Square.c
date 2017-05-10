32525b6209ed47a71c088e22ac3a9fd345c4515f
Al-Amin <ialamin.pro@gmail.com>
2017-05-11 00:48:23 +0600

    1022-Circle in Square.c

---
+++ 1022-Circle in Square.c
@@ -0,0 +1,24 @@
+/************************************************************************
+***   Problem No    : 1020                                            ***
+***   Problem Name  : A Childhood Game                                ***
+***   Type          : Ad-hoc                                          ***
+***   Author        : Al-Amin                                         ***
+***   E-mail        : ialamin.pro@gmail.com/alamin.iyakub@diu.edu.bd  ***
+***   University    : DIU, Dept. of CSE                               ***
+***   Facebook      : http://www.facebook.com/alamin25                ***
+************************************************************************/
+#include<stdio.h>
+int main()
+{
+    int T,i;
+     double a,b,res,r,PI=2*acos(0.0);
+     scanf("%d",&T);
+     for(i=1;i<=T;i++)
+   {
+    scanf("%lf",&r);
+    a=r*2;
+    res=(a*a)-(PI*r*r);
+    printf("Case %d: %0.2lf\n",i,res);
+   }
+return 0;
+}
